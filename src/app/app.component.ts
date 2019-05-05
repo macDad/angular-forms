2001f3d85e3e6f5d76ba79f07240b0d353d853b7
Jenkins <jaya.maduka@gmail.com>
2019-05-05 16:38:33 +0800

    Add Remove Save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      published: true,
+      credentials: this.fb.array([]),
+    });
+  }
+
+  addCreds() {
+    const creds = this.form.controls.credentials as FormArray;
+    creds.push(this.fb.group({
+      username: '',
+      password: '',
+    }));
+  }
+
+  removeCreds() {
+    const creds = this.form.controls.credentials as FormArray;
+    creds.removeAt(0);
+  }
+
+  removeAllCreds() {
+    const creds = this.form.controls.credentials as FormArray;
+      while (0 !== creds.length) {
+        creds.removeAt(0);
+      }
+  }
+
+  removeRow(index) {
+    const creds = this.form.controls.credentials as FormArray;
+    creds.removeAt(index);
+  }
+
+  saveCreds() {
+    console.log(this.form.value);
+    const creds = this.form.controls.credentials as FormArray;
+    this.title = this.form.value;
+  }
 }
